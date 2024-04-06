6e1d8b624574be820356ad46d2e4573eb5f29ae6
Roberto Perales <grupo2ingsoftware2@outlook.com>
2024-04-06 08:57:57 -0400

    Examen a medias

---
+++ Examen02/Api.Tienda/Implementacion/ClienteLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Tienda.Implementacion
+{
+    public class ClienteLogic
+    {
+
+        public async Task<bool> InsertarPersona(ClienteLogic cliente)
+        {
+            try
+            {
+                contexto.Cliente.Add(cliente);
+                int response = await contexto.SaveChangesAsync();
+                return response == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
